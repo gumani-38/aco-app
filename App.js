11e85a3c6a579cf8434df14639bf3621bbfc082d
gumani-38 <surprisegumani@gmail.com>
2024-07-05 11:09:59 +0200

    auth provider removed

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import "react-native-gesture-handler";
 import StackNavigation from "./app/navigation/StackNavigation";
-import { AuthProvider } from "./app/context/AuthContext";
 import { ModalPortal } from "react-native-modals";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <BottomSheetModalProvider>
-          <StackNavigation />
-          <ModalPortal />
-        </BottomSheetModalProvider>
-      </GestureHandlerRootView>
-    </AuthProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
+        <StackNavigation />
+        <ModalPortal />
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 }
